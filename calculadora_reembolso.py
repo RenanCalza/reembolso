8ea57aca1514c1ac11408080d0830b9d7c287839
RenanCalza <renan.calza@hotmail.com>
2026-05-04 14:07:53 -0300

    add fuel options

--- calculadora_reembolso.py
+++ calculadora_reembolso.py
@@ -1,21 +1,29 @@
 import streamlit as st
 st.title('Calculadora de Reembolso de combustível ⛽')
-#st.subheader('Calza')
+st.subheader('Escola o combustível ')
 # Quando vale a pena escolher etanol ao inves de gasolina?
 etanol = 0.36
 gasolina = 0.25
+opcoes = ['Gasolina','Etanol']
 #vEtanol = 0.0
 #vGasolina = 0.0
 #totalKm = 0.0
+escolha = st.selectbox('Escolha o combustível\n',opcoes)
 
-vEtanol = st.number_input('Digite o valor do etanol', min_value = 0.0)
-totalKm = st.number_input('Quantos Km você rodou no total? ',min_value =0.0)
+
+if escolha == 'Etanol':
+    vEtanol = st.number_input('Digite o valor do etanol', min_value = 0.0)
+    totalKm = st.number_input('Quantos Km você rodou no total? ',min_value =0.0)
+    vKm = etanol * vEtanol
 #gasolina = st.number_input('Digite o valor da gasolina', min_value = 0.0)
+else:
+    vGasolina = st.number_input('Digite o valor da Gasolina', min_value = 0.0)
+    totalKm = st.number_input('Quantos Km você rodou no total? ',min_value =0.0)
+    vKm = gasolina * vGasolina
 
-vKm = etanol * vEtanol
 reembolso = vKm *totalKm
 if st.button('Resolver'):
-    st.success(f'{reembolso:.2f}')
+    st.success(f'Valor do reembolso:\n R$ {reembolso:.2f}')
 #----------------------------------------------------------------------------
 #if gasolina >0:
 #   resultado = etanol/gasolina 
